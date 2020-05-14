c4582613196f038a3a0e99eecaf6f4d39562f314
Thabata Marchi <thabatamarchi@Mac-mini-de-Thabata.local>
2020-05-14 16:34:36 -0300

    exemplo de AsyncStorage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -8,12 +8,31 @@ import {
   Modal,
   Button,
   SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
 } from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
   const [sliderValue, setSlider] = useState(0);
   const [switchStatus, setSwitchStatus] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
+
+  const [inputAsync, setInputAsync] = useState('');
+  const [nomeInput, setNomeInput] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('nome').then((value) => {
+      setNomeInput(value);
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.setItem('nome', nomeInput);
+  }, [nomeInput]);
 
   const entrar = () => {
     setModalVisible(true);
@@ -23,41 +42,73 @@ const App = () => {
     setModalVisible(false);
   };
 
+  const gravaNome = () => {
+    setNomeInput(inputAsync);
+    Keyboard.dismiss();
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <View style={styles.box}>
-          <Slider
-            minimumValue={0}
-            maximumValue={100}
-            onValueChange={(value) => setSlider(value)}
-            value={sliderValue}
-            minimumTrackTintColor="#f04e23"
-            maximumTrackTintColor="#eee"
-            thumbTintColor="#f04e23"
-          />
-          <Text style={styles.textSlider}>{sliderValue.toFixed(1)}</Text>
-        </View>
-        <View style={styles.box}>
-          <Switch
-            value={switchStatus}
-            onValueChange={(value) => setSwitchStatus(value)}
-            thumbColor="#0c773a"
-          />
-          <Text style={styles.textSwitch}>
-            {switchStatus ? 'Ativo' : 'Inativo'}
-          </Text>
-        </View>
-        <View style={styles.box}>
-          <Button style={styles.btnModal} title="Entrar" onPress={entrar} />
-        </View>
-
-        <Modal animationType="slide" visible={modalVisible}>
-          <View style={styles.modal}>
-            <Text style={styles.textModal}>Seja Bem-vindo!</Text>
-            <Button style={styles.btnModal} title="Sair" onPress={sair} />
+        {/* Exemplo de Slider */}
+        <ScrollView>
+          <View style={styles.box}>
+            <Slider
+              minimumValue={0}
+              maximumValue={100}
+              onValueChange={(value) => setSlider(value)}
+              value={sliderValue}
+              minimumTrackTintColor="#f04e23"
+              maximumTrackTintColor="#eee"
+              thumbTintColor="#f04e23"
+            />
+            <Text style={styles.textSlider}>{sliderValue.toFixed(1)}</Text>
           </View>
-        </Modal>
+
+          {/* Exemplo de Switch */}
+          <View style={styles.box}>
+            <Switch
+              value={switchStatus}
+              onValueChange={(value) => setSwitchStatus(value)}
+              thumbColor="#0c773a"
+            />
+            <Text style={styles.textSwitch}>
+              {switchStatus ? 'Ativo' : 'Inativo'}
+            </Text>
+          </View>
+
+          {/* Exemplo de Modal */}
+          <View style={styles.box}>
+            <Button style={styles.btnModal} title="Entrar" onPress={entrar} />
+          </View>
+          <Modal
+            transparent={true}
+            animationType="slide"
+            visible={modalVisible}>
+            <View style={styles.modalCentered}>
+              <View style={styles.modal}>
+                <Text style={styles.textModal}>Seja Bem-vindo!</Text>
+                <Button style={styles.btnModal} title="Sair" onPress={sair} />
+              </View>
+            </View>
+          </Modal>
+
+          {/* Exemplo de AsyncStorage */}
+          <View style={styles.box}>
+            <View style={styles.viewInput}>
+              <TextInput
+                style={styles.input}
+                value={inputAsync}
+                onChangeText={(text) => setInputAsync(text)}
+                underlineColorAndroid="transparent"
+              />
+              <TouchableOpacity onPress={gravaNome}>
+                <Text style={styles.botao}>+</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Text style={styles.nome}>{nomeInput}</Text>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -77,19 +128,36 @@ const styles = StyleSheet.create({
     shadowColor: '#000000',
     elevation: 2,
   },
-  modal: {
-    backgroundColor: '#292929',
+  modalCentered: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modal: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   btnModal: {
     fontSize: 20,
     backgroundColor: '#F1f1f1',
   },
   textModal: {
-    color: '#FFF',
+    color: '#333',
     textAlign: 'center',
     fontSize: 30,
+    marginBottom: 30,
   },
   textSlider: {
     textAlign: 'center',
@@ -98,6 +166,31 @@ const styles = StyleSheet.create({
   textSwitch: {
     textAlign: 'center',
     fontSize: 30,
+  },
+
+  viewInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 10,
+  },
+  botao: {
+    backgroundColor: '#222',
+    color: '#FFF',
+    height: 40,
+    padding: 10,
+    marginLeft: 4,
+  },
+  nome: {
+    marginTop: 5,
+    fontSize: 30,
+    textAlign: 'center',
+    flex: 1,
   },
 });
 
