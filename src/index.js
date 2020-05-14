acc1681b94d172664ad9196531105537fabbf942
Thabata Marchi <thabatamarchi@Mac-mini-de-Thabata.local>
2020-05-14 09:36:24 -0300

    Componente Switch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Slider} from 'react-native';
+import {View, Text, StyleSheet, Slider, Switch} from 'react-native';
 
 const App = () => {
   const [sliderValue, setSlider] = useState(0);
+  const [switchStatus, setSwitchStatus] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <Slider
-        minimumValue={0}
-        maximumValue={100}
-        onValueChange={(value) => setSlider(value)}
-        value={sliderValue}
-        minimumTrackTintColor="#f04e23"
-        maximumTrackTintColor="#eee"
-      />
-      <Text style={styles.textSlider}>{sliderValue.toFixed(1)}</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Slider
+            minimumValue={0}
+            maximumValue={100}
+            onValueChange={(value) => setSlider(value)}
+            value={sliderValue}
+            minimumTrackTintColor="#f04e23"
+            maximumTrackTintColor="#eee"
+            thumbTintColor="#f04e23"
+          />
+          <Text style={styles.textSlider}>{sliderValue.toFixed(1)}</Text>
+        </View>
+        <View style={styles.box}>
+          <Switch
+            value={switchStatus}
+            onValueChange={(value) => setSwitchStatus(value)}
+            thumbColor="#0c773a"
+          />
+          <Text style={styles.textSwitch}>
+            {switchStatus ? 'Ativo' : 'Inativo'}
+          </Text>
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -23,8 +39,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 60,
+    backgroundColor: '#F3F3F3',
+  },
+  box: {
+    margin: 20,
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: '#FFF',
   },
   textSlider: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  textSwitch: {
     textAlign: 'center',
     fontSize: 30,
   },
