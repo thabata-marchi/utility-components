2b6ce9ea0a9a33284e50c6260dd5720c7edc53c4
Thabata Marchi <thabatamarchi@Mac-mini-de-Thabata.local>
2020-05-14 11:22:19 -0300

    Modal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Slider, Switch} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Slider,
+  Switch,
+  Modal,
+  Button,
+  SafeAreaView,
+} from 'react-native';
 
 const App = () => {
   const [sliderValue, setSlider] = useState(0);
   const [switchStatus, setSwitchStatus] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const entrar = () => {
+    setModalVisible(true);
+  };
+
+  const sair = () => {
+    setModalVisible(false);
+  };
 
   return (
     <>
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <View style={styles.box}>
           <Slider
             minimumValue={0}
@@ -30,7 +48,17 @@ const App = () => {
             {switchStatus ? 'Ativo' : 'Inativo'}
           </Text>
         </View>
-      </View>
+        <View style={styles.box}>
+          <Button style={styles.btnModal} title="Entrar" onPress={entrar} />
+        </View>
+
+        <Modal animationType="slide" visible={modalVisible}>
+          <View style={styles.modal}>
+            <Text style={styles.textModal}>Seja Bem-vindo!</Text>
+            <Button style={styles.btnModal} title="Sair" onPress={sair} />
+          </View>
+        </Modal>
+      </SafeAreaView>
     </>
   );
 };
@@ -39,13 +67,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 60,
-    backgroundColor: '#F3F3F3',
   },
   box: {
     margin: 20,
     padding: 20,
-    borderRadius: 5,
+    flexDirection: 'column-reverse',
     backgroundColor: '#FFF',
+    borderBottomWidth: 0.2,
+    shadowColor: '#000000',
+    elevation: 2,
+  },
+  modal: {
+    backgroundColor: '#292929',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  btnModal: {
+    fontSize: 20,
+    backgroundColor: '#F1f1f1',
+  },
+  textModal: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 30,
   },
   textSlider: {
     textAlign: 'center',
